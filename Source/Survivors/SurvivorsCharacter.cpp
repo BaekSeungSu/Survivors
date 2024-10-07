2c20efc9cfd5073e0a5d131f23977b12222d9271
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-10-07 19:59:58 +0900

    주석 삭제

--- Source/Survivors/SurvivorsCharacter.cpp
+++ Source/Survivors/SurvivorsCharacter.cpp
@@ -5,31 +5,26 @@
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
-// Sets default values
 ASurvivorsCharacter::ASurvivorsCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
     GetCharacterMovement()->bOrientRotationToMovement = true;
     bUseControllerRotationYaw = false;
 }
 
-// Called when the game starts or when spawned
 void ASurvivorsCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void ASurvivorsCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
     Turn(DeltaTime);
 }
 
-// Called to bind functionality to input
 void ASurvivorsCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
