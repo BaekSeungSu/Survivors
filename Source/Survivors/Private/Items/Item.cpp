a01f1d30818a9af2e73f70ec5c74a7f13b0d0f4b
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-10-13 22:22:24 +0900

    아이템 클래스 테스트

--- Source/Survivors/Private/Items/Item.cpp
+++ Source/Survivors/Private/Items/Item.cpp
@@ -13,11 +13,25 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Red, TEXT("Print String to Screen"));
+	}
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime : %f"), DeltaTime);
+
+	if(GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name : %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Red, Message);
+		UE_LOG(LogTemp, Warning, TEXT("Item Name : %s"), *Name);
+	}
 
 }
 
