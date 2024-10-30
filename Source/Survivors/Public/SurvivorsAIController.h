44589b88024de270e402f6607f4942ab9606c63a
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-10-30 20:22:14 +0900

    AI컨트롤러 추가

---
+++ Source/Survivors/Public/SurvivorsAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SurvivorsAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVORS_API ASurvivorsAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
